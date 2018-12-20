a91b1acee78cb20ded81e5bab71dbee17ce1f86e
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2018-12-20 20:24:20 +0000

    La page Single-appareil prend en paramètre un objet appareil

--- src/pages/appareils/single-appareil/single-appareil.ts
+++ src/pages/appareils/single-appareil/single-appareil.ts
@@ -1,26 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SingleAppareilPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @Component({
   selector: 'page-single-appareil',
   templateUrl: 'single-appareil.html',
 })
 export class SingleAppareilPage implements OnInit {
 
-  name: string;
+  appareil: {
+    name: string,
+    description: string[]
+  };
+
 
   constructor(public navParams: NavParams) {
   }
 
   ngOnInit(){
-    this.name = this.navParams.get('appareilName');
+    this.appareil = this.navParams.get('appareil');
  }
 
 }
