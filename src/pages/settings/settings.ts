54c7285c0928bc4dec2b98e2528e3637ed1cc413
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2018-12-21 10:59:44 +0000

    Création d'alert dans la page settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the SettingsPage page.
@@ -14,11 +14,26 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public alertCtrl: AlertController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
+  onToggleLights(){
+    let alert = this.alertCtrl.create({
+      title: 'Êtes-vous certain(e) de vouloir continuer?',
+      subTitle: 'Cette action allumera ou éteindra toutes les lumières de la maison !',
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel'
+        },
+        {
+          text: 'Confirmer',
+          handler: () => console.log('Confirmé !')
+        }
+      ]
+    });
+
+    alert.present();
   }
 
 }
